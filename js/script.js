4e8bae5b480f53f476f956ec7bf78ae366904f56
XaydarovSardor <birbalobirbattar@gmail.com>
2024-10-09 16:33:57 +0500

    some secitons added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 AOS.init();
+var swiper = new Swiper(".blogSwiper", {
+    slidesPerView:3,
+    spaceBetween:14,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+var swiper = new Swiper(".eventSwiper", {
+    spaceBetween:15,
+    navigation: {
+        nextEl: ".swiper-button-nextt",
+        prevEl: ".swiper-button-prevv",
+    },
+});
