d90705fff791a1bf6b0184d07459062ade5201ca
XaydarovSardor <birbalobirbattar@gmail.com>
2024-10-29 23:16:19 +0500

    some sections added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,28 @@
 AOS.init();
 var swiper = new Swiper(".blogSwiper", {
-    slidesPerView:3,
-    spaceBetween:14,
+    breakpoints: {
+        360:{
+            slidesPerView: 1,
+            spaceBetween: 14,
+        },
+        500:{
+            slidesPerView: 1.5,
+            spaceBetween: 14,
+        },
+        768:{
+            slidesPerView: 2,
+            spaceBetween: 14,
+        },
+        992: {
+            slidesPerView: 2,
+            spaceBetween: 14,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 14,
+
+        },
+    },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -9,9 +30,25 @@ var swiper = new Swiper(".blogSwiper", {
 });
 
 var swiper = new Swiper(".eventSwiper", {
-    spaceBetween:15,
+    spaceBetween: 15,
     navigation: {
         nextEl: ".swiper-button-nextt",
         prevEl: ".swiper-button-prevv",
     },
 });
+
+// tabs 
+const tabsTitle = document.querySelectorAll(".tab-link");
+tabsContent = document.querySelectorAll(".tab-content");
+tabsContent.forEach(item => item.classList.add("tab-hidden"))
+tabsTitle.forEach(item => item.addEventListener("click", event => {
+    const tabTitleTarget = event.target.getAttribute("data-tab");
+    console.log(item)
+    tabsTitle.forEach(element => element.classList.remove("active"));
+    tabsContent.forEach(element => element.classList.add("tab-hidden"));
+    item.classList.add("active");
+    document.getElementById(tabTitleTarget).classList.remove("tab-hidden");
+}))
+
+document.querySelector('[data-tab="tab-1"]').classList.add("active");
+document.querySelector('#tab-1').classList.remove("tab-hidden");
